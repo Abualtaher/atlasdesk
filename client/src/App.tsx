08399db9f2216c6df44dfd5460fc44724de829c3
abualtaher <wisamaltaher@gmail.com>
2026-09-08 19:38:50 +0200

    feat: refactor App component to streamline routing and remove Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
 
 import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
+import Login from "./pages/Login";
 import Conversations from "./pages/Conversations";
 import Tickets from "./pages/Tickets";
 import Settings from "./pages/Settings";
+import Customers from "./pages/Customers";
 import CustomerProfile from "./pages/CustomerProfile";
-import Login from "./pages/Login";
+import AppLayout from "./components/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex min-h-screen bg-gray-100">
-        <Sidebar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-        <main className="flex-1 p-8">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/customers/:id" element={<CustomerProfile />} />
-            <Route path="/conversations" element={<Conversations />} />
-            <Route path="/tickets" element={<Tickets />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-      </div>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/customers/:id" element={<CustomerProfile />} />
+          <Route path="/conversations" element={<Conversations />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
